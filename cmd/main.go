96f69908d0b69c9c0fa4e5c160f0d7741b20547b
etiryaki <emretiryaki3@gmail.com>
2018-08-14 08:06:55 +0300

    adding server and service in backend

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"context"
+
+		)
+
+var version = "1.0.0"
+var commit = "NA"
+
+
+var configFile = flag.String("config","","path config file")
+func  main(){
+
+	v := flag.Bool("v", false, "prints current version and exits")
+
+
+	flag.Parse()
+
+	if *v {
+		fmt.Printf("Version %s (commit: %s)\n", version, commit)
+		os.Exit(0)
+	}
+
+
+
+}
+
+type MerkutServerImpl struct {
+	context            context.Context
+	shutdownFn         context.CancelFunc
+	//childRoutines      *errgroup.Group
+	//log                log.Logger
+	//cfg                *setting.Cfg
+	shutdownReason     string
+	shutdownInProgress bool
+
+	//RouteRegister routing.RouteRegister `inject:""`
+	//HttpServer    *api.HTTPServer       `inject:""`
+}
