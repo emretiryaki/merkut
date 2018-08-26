abaef30e397a4d19213255063d409cb98556dbcd
etiryaki <emretiryaki3@gmail.com>
2018-08-26 14:45:33 +0300

    change from ts to react js

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello, I am Merkur !
+      </div>
+    );
+  }
+}
+
+export default App;
